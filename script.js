74e97015c73510c7b35d1688e2b49514d652ef66
Gene McDaniel <79287709+GeneMcDaniel@users.noreply.github.com>
2022-04-08 08:56:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.querySelector("#home-btn").onclick = () => {
+  window.location = "index.html";
+}
 
+document.querySelector("#book-btn").onclick = () => {
+  window.location = "books.html";
+}
