408d9b733ebe9de07b1ece718a5328c8daab2ff1
Gene McDaniel <79287709+GeneMcDaniel@users.noreply.github.com>
2022-04-08 09:00:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelector("#home-btn").onclick = () => {
+document.querySelector("#home-btn").addEventListener("click", () => {
   window.location = "index.html";
-}
+});
 
-document.querySelector("#book-btn").onclick = () => {
+document.querySelector("#book-btn").addEventListener("click", () => {
   window.location = "books.html";
-}
+});
